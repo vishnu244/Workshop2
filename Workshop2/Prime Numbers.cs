5a59d09ee22c91606f0f8c9eb84ce1b5804b9663
vishnu244 <vishnunali244@gmail.com>
2022-06-27 09:25:48 +0530

    [vishnu]-Prime Number logic (Workshop2)

---
+++ Workshop2/Prime Numbers.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Workshop2
+{
+    public class Prime_Numbers
+    {
+        public void primeNumber()
+        {
+            int num, i, count, start, end;
+
+            Console.Write("start number:");
+            start = Convert.ToInt32(Console.ReadLine());
+            Console.Write("End Number:");
+            end = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("Prime Numbers Between {0} and {1} are: ",start,end);
+
+            for (num = start; num <= end; num++)
+            {
+                count = 0;
+
+                for (i = 2; i <= num/2; i++)
+                {
+                    if (num % i == 0)
+                    {
+                        count++;
+
+                    }
+                }
+                if (count == 0 && num != 1)
+                    Console.Write("{0} ",num);
+            }
+        }
+    }
+}
---
+++ Workshop2/Program.cs
@@ -0,0 +1,7 @@
+﻿// See https://aka.ms/new-console-template for more information
+using Workshop2;
+
+Console.WriteLine("Workshop2!");
+
+Prime_Numbers prime_Numbers = new Prime_Numbers();
+prime_Numbers.primeNumber();
